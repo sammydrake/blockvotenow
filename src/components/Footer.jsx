8a1fad17494817127e9cce4109740b9819474ca2
drakesammy <drakesammy0101@gmail.com>
2023-01-27 02:52:11 -0600

    new changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
-import { AiFillGithub } from 'react-icons/ai'
-import { CgYoutube } from 'react-icons/cg'
-import { FaTwitter } from 'react-icons/fa'
-import { GrLinkedin } from 'react-icons/gr'
+import { AiFillGithub } from "react-icons/ai";
+import { CgYoutube } from "react-icons/cg";
+import { FaTwitter } from "react-icons/fa";
+import { GrLinkedin } from "react-icons/gr";
 
 const Footer = () => {
   return (
     <footer className="text-center bg-gray-900 text-white mt-20">
       <div className="flex justify-center items-center mx-auto space-x-3 p-4">
         <a
-          href="https://www.linkedin.com/in/darlington-gospel-aa626b125/"
+          href="https://www.linkedin.com/"
           target="_blank"
           type="button"
           className="rounded-full text-white leading-normal uppercase hover:bg-black
@@ -18,7 +18,7 @@ const Footer = () => {
           <GrLinkedin size={35} />
         </a>
         <a
-          href="https://www.youtube.com/@dappmentors?sub_confirmation=1"
+          href="https://www.youtube.com"
           target="_blank"
           type="button"
           className="rounded-full text-white leading-normal uppercase hover:bg-black
@@ -29,7 +29,7 @@ const Footer = () => {
         </a>
 
         <a
-          href="https://github.com/Daltonic"
+          href="https://github.com/"
           target="_blank"
           type="button"
           className="rounded-full text-white leading-normal uppercase hover:bg-black
@@ -39,7 +39,7 @@ const Footer = () => {
           <AiFillGithub size={35} />
         </a>
         <a
-          href="https://twitter.com/iDaltonic"
+          href="https://twitter.com"
           target="_blank"
           type="button"
           className="rounded-full text-white leading-normal uppercase hover:bg-black
@@ -51,19 +51,19 @@ const Footer = () => {
       </div>
       <div
         className="flex flex-col justify-center items-center text-center p-4"
-        style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
         © 2022
         <a
           className="flex space-x-2 text-whitehite"
-          href="https://daltonic.github.io/"
+          href="https://grandvote.com/"
         >
           <span>With Love ❤️</span>
-          <span className="text-orange-700">Daltonic</span>
+          <span className="text-orange-700">Grandida</span>
         </a>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
